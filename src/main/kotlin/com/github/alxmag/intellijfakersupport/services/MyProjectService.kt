be6cbac9664fbd85b402581f2af98427b8e175ba
GitHub Action <action@github.com>
2023-01-08 19:18:35 +0000

    Template cleanup

--- src/main/kotlin/com/github/alxmag/intellijfakersupport/services/MyProjectService.kt
+++ src/main/kotlin/com/github/alxmag/intellijfakersupport/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.alxmag.intellijfakersupport.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.alxmag.intellijfakersupport.MyBundle
 
 class MyProjectService(project: Project) {
 
