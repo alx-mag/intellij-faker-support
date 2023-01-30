53c7cb6372e59b554302d111a8b0f5eea844f195
alx.mag <alx.serg.mag@gmail.com>
2023-01-30 22:40:29 +0400

    Closing brace auto-insert

--- src/main/kotlin/com/github/alxmag/intellijfakersupport/lang/editor/FakerTypedHandler.kt
+++ src/main/kotlin/com/github/alxmag/intellijfakersupport/lang/editor/FakerTypedHandler.kt
@@ -1,9 +1,36 @@
 package com.github.alxmag.intellijfakersupport.lang.editor
 
+import com.github.alxmag.intellijfakersupport.lang.psi.FakerFile
+import com.intellij.codeInsight.CodeInsightSettings
 import com.intellij.codeInsight.editorActions.TypedHandlerDelegate
+import com.intellij.openapi.editor.Editor
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.util.TextRange
+import com.intellij.psi.PsiFile
 
-// TODO
 class FakerTypedHandler : TypedHandlerDelegate() {
+    override fun charTyped(c: Char, project: Project, editor: Editor, file: PsiFile): Result {
+        if (file is FakerFile) {
+            processBrace(c, editor)
+        }
+        return super.charTyped(c, project, editor, file)
+    }
 
+    // Complete '#{' to '#{}'
+    private fun processBrace(c: Char, editor: Editor) {
+        if (!CodeInsightSettings.getInstance().AUTOINSERT_PAIR_BRACKET) return
+        if (c == '{') {
+            val caretOffset = editor.caretModel.offset
+            // The typed brace is the very first char of the file
+            if (caretOffset <= 1) return
+
+            val document = editor.document
+            // Check if brace typed after '#'
+            val isBeforeHash = document.getText(TextRange(caretOffset - 2, caretOffset - 1)) == "#"
+            if (isBeforeHash) {
+                document.insertString(caretOffset, "}")
+            }
+        }
+    }
 }
 
--- src/test/kotlin/com/github/alxmag/intellijfakersupport/lang/editor/FakerTypedHandlerTest.kt
+++ src/test/kotlin/com/github/alxmag/intellijfakersupport/lang/editor/FakerTypedHandlerTest.kt
@@ -15,7 +15,18 @@ class FakerTypedHandlerTest : BasePlatformTestCase() {
         "my'<caret>Text#{foo}"
     )
 
+    fun testBraceAfterHash() = doBraceTest(
+        "text#<caret>",
+        "text#{<caret>}"
+    )
+
+    fun `test Brace at Start of the String`() = doBraceTest(
+        "<caret>",
+        "{<caret>"
+    )
+
     private fun doQuoteTest(before: String, after: String) = doTypeTest("'", before, after)
+    private fun doBraceTest(before: String, after: String) = doTypeTest("{", before, after)
 
     private fun doTypeTest(type: String, before: String, after: String) {
         myFixture.configureByText(FakerFileType.INSTANCE, before)
